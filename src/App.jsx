7dad03add51aed9948e9d6cb463998ca92dadb01
Ayush <invinciblefoxstar@gmail.com>
2025-07-06 17:54:03 +0530

    updated handling cart and wishlist logic to handle duplicate requests

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,33 @@ function App() {
   const [wishlist, setWishlist] = useState([]);
 
   const handleAddToCart = (product) => {
-    setCart([...cart, product]);
+    const existingProduct = cart.find(item => item.id === product.id)
+
+    if (existingProduct) {
+      setCart(cart.map(item => 
+        item.id === product.id
+        ? {...item, quantity : item.quantity + 1}
+        : item
+      ));
+    } else {
+      setCart([...cart, {...product, quantity: 1}]);
+    }
     console.log('Product added to cart:', product);
+    console.log('Current cart:', cart);
   };
 
   const handleAddToWishlist = (product)=>{
-    setWishlist([...wishlist, product]);
-    console.log('Product added to wishlist:', product);
-  };
+    const isWishlisted = wishlist.find(item => item.id === product.id)
+
+    if (isWishlisted){
+      setWishlist(wishlist.filter(item => item.id !== product.id));
+      console.log("Product removed:",product);
+    }
+    else {
+      setWishlist([...wishlist, product]);
+      console.log("Product added to wishlist",product);
+    }
+  }
 
 
 

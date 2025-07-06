467bd663d0d1f19141e3445eee3d63a12c7780ee
Ayush <invinciblefoxstar@gmail.com>
2025-07-06 16:55:32 +0530

    Handle addToCart and addToWishlist and defined cart and wishlist states

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css'
 import {Outlet} from 'react-router-dom'
+import {useState} from 'react';
 
 import Navbar from './components/Navbar'
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [wishlist, setWishlist] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCart([...cart, product]);
+    console.log('Product added to cart:', product);
+  };
+
+  const handleAddToWishlist = (product)=>{
+    setWishlist([...wishlist, product]);
+    console.log('Product added to wishlist:', product);
+  };
+
+
 
   return (
     <>
-      <Navbar />
+      <Navbar cartCount = {cart.length} wishlistCount = {wishlist.length}/>
     <main>
-      <Outlet />
+      <Outlet context ={{  handleAddToCart, handleAddToWishlist }}/>
     </main>
     </>
   )

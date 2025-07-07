5e76b8b583be79007f4b992b11ac7ad4642a34c5
Ayush <invinciblefoxstar@gmail.com>
2025-07-07 17:56:37 +0530

    New title and icon wihth attributions in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,3 @@
-
-
 function Footer() {
   const currentYear = new Date().getFullYear();
 
@@ -9,6 +7,15 @@ function Footer() {
         <p>&copy; {currentYear} FakeStore. All Rights Reserved.</p>
         <p className="text-sm text-gray-400 mt-2">
           A Project for The Odin Project
+        </p>
+        <p className="text-xs text-gray-500 mt-2">
+          <a
+            href="https://www.flaticon.com/free-icons/spike"
+            title="spike icons"
+            className="underline"
+          >
+            Spike icons created by Freepik - Flaticon
+          </a>
         </p>
       </div>
     </footer>

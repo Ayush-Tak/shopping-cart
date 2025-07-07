446bd953466b9def3a865ff8a6fb365bd3f0911f
Ayush <invinciblefoxstar@gmail.com>
2025-07-07 13:11:19 +0530

    fixed loading on side

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -28,14 +28,15 @@ function ShopPage() {
 
   if (loading)
     return (
-      <>
+      <div className="flex items-center justify-center min-h-screen">
         <div className="flex flex-row gap-2">
           <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
           <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
           <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
         </div>
-      </>
+      </div>
     );
+
 
   if (error)
     return (
